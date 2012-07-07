0aac9d4bdd0f4eb90476a65c9915756351a0a447
Ervin Kosch <ervin.kosch@gmail.com>
2012-07-06 22:15:19 -0400

    Initial Offering
    First version

---
+++ eTable.js
@@ -0,0 +1,45 @@
+/*
+
+** eTable **
+A single function to build a table based on a JSON object
+
+*Disclaimer*
+This code is provided without warranty. While I strive to maintain backwards compatibility with previous versions, the code is still under active development. As this is the case, some revisions may break compatibility with earlier versions of the library. Please keep this in mind when using eTable.
+
+*Copyright and Licensing*
+Copyright (c) 2012 Ervin Kosch, released under the GPL 3 license
+
+*Updated Source Code and Instructions:*
+	https://github.com/tekrat/eTable
+
+*/
+function eTab(d,i,t,h,c){
+	var r = "<table";
+	if(typeof i == "undefined"){ r += ' id="' + i + '"'; }
+	if(typeof t == "undefined"){ r += ' class="' + t + '"'; }
+	r += "<tr>\r\n"	
+	for (var k in d.header) {
+		r += "	<th";
+		if(typeof h == "undefined"){ r += ' class="' + h + '"'; }
+		r += "	>" + d.header[k] + "</th>\r\n";
+	}
+	r += "</tr>\r\n"
+	r += "<tbody>\r\n"
+		
+	for(x in d.rows){
+		r += "<tr>\r\n";		
+		q = 0;
+		for (var k in d.header) {			
+			r += "	<td";
+			if(typeof c == "undefined"){ r += ' class="' + c + '"'; }
+			r += ">" + d.rows[x][q] + "</td>\r\n";
+			q++;
+		}
+		r += "</tr>\r\n";
+	
+	}	
+	r += "</tbody>\r\n"
+	r += "</table>\r\n"
+	
+	return r;
+}
