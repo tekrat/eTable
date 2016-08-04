1ed95d672f64ec576309e1d8ddd47dfcd68ee224
Ervin Kosch <ervin.kosch@gmail.com>
2016-08-04 09:15:15 -0400

    THEAD tag noe properly forms, ID element no longer have extra spaces

--- eTable.js
+++ eTable.js
@@ -7,7 +7,7 @@ A single function to build a table based on a JSON object
 This code is provided without warranty. While I strive to maintain backwards compatibility with previous versions, the code is still under active development. As this is the case, some revisions may break compatibility with earlier versions of the library. Please keep this in mind when using eTable.
 
 *Copyright and Licensing*
-Copyright (c) 2012 Ervin Kosch, released under the GPL 3 license
+Copyright (c) 2012, 2016 Ervin Kosch, released under the GPL 3 license
 
 *Updated Source Code and Instructions:*
 	https://github.com/tekrat/eTable
@@ -17,6 +17,8 @@ function eTab(d,i,t,h,c){
 	var r = "<table";
 	if(typeof i != "undefined"){ r += ' id="' + i + '"'; }
 	if(typeof t != "undefined"){ r += ' class="' + t + '"'; }
+  r += ">\r\n"
+  r += "<thead>\r\n"	
 	r += "<tr>\r\n"	
 	for (var k in d.header) {
 		r += "	<th";
@@ -24,10 +26,10 @@ function eTab(d,i,t,h,c){
 		r += "	>" + d.header[k] + "</th>\r\n";
 	}
 	r += "</tr>\r\n"
+	r += "</thead>\r\n"
 	r += "<tbody>\r\n"
-		
 	for(x in d.rows){
-		r += '<tr id="' + x + ' ">\r\n';		
+		r += '<tr id="' + x + '">\r\n';		
 		q = 0;
 		for (var k in d.header) {			
 			r += '	<td id="' + x + "_" + k  + '"';
@@ -43,3 +45,15 @@ function eTab(d,i,t,h,c){
 	
 	return r;
 }
+
+
+
+document.write(
+  eTab({
+    header:["a", "b", "c"],
+        rows:{
+            row1:["1", "2", "3"],
+            row2:["q", "w", "e"]
+        }
+    })
+);
